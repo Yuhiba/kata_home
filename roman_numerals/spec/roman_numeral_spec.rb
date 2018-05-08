1f79eb9153164f633a8522053021f902bd099c5a
Yuhiba <elodie.pelcat@gmail.com>
2018-05-08 17:24:44 +0200

    feat<Change VI in 6> We can change VI in 6

--- roman_numerals/spec/roman_numeral_spec.rb
+++ roman_numerals/spec/roman_numeral_spec.rb
@@ -1,17 +1,24 @@
 require_relative '../roman_numeral'
 
-RSpec.describe "A function of conversion from Roman number to Arabic number" do
-  it "Change I in 1" do
-    valeur = 'I'
-    valeur_expect = 1
-    result = roman_numeral(valeur)
-    expect(result).to eq(valeur_expect)
+RSpec.describe 'A function of conversion from Roman number to Arabic number' do
+  it 'Change I in 1' do
+    value = 'I'
+    value_expect = 1
+    result = roman_numeral(value)
+    expect(result).to eq(value_expect)
   end
 
-  it "Change V in 5" do
-    valeur = 'V'
-    valeur_expect = 5
-    result = roman_numeral(valeur)
-    expect(result).to eq(valeur_expect)
+  it 'Change V in 5' do
+    value = 'V'
+    value_expect = 5
+    result = roman_numeral(value)
+    expect(result).to eq(value_expect)
+  end
+
+  it 'Change VI in 6' do
+    value = 'VI'
+    value_expect = 6
+    result = roman_numeral(value)
+    expect(result).to eq(value_expect)
   end
 end
