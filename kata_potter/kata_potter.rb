cd4bef48a9936db67ee9a0d4656ceb3f4a2a8cb4
Yuhiba <elodie.pelcat@gmail.com>
2018-05-10 16:17:22 +0200

    feat(Calculate different books) : Nous pouvons calculer le prix totale et sans réduction pour différents livres achetés.

--- kata_potter/kata_potter.rb
+++ kata_potter/kata_potter.rb
@@ -1,5 +1,6 @@
-def magic_calculator(nb_books)
-  return 8 if nb_books == 1
-  return 16 if nb_books == 2
+def different_books(nb_books)
+  return 8 * nb_books
+end
 
+def magic_calculator(nb_books)
 end
--- kata_potter/spec/kata_potter_spec.rb
+++ kata_potter/spec/kata_potter_spec.rb
@@ -1,17 +1,42 @@
 require_relative '../kata_potter'
 
 RSpec.describe "A Magic Calculator function that calculates the discount on Harry Potter books." do
-  it "a book costs 8 euros" do
-    nb_books = 1
-    value_expect = 8
-    result = magic_calculator(nb_books)
-    expect(result).to eq value_expect
+  context "Calculate different books" do
+    it "One book costs 8 euros" do
+      nb_books = 1
+      value_expect = 8
+      result = different_books(nb_books)
+      expect(result).to eq value_expect
+    end
+
+    it "Two different books cost 16 euros" do
+      nb_books = 2
+      value_expect = 16
+      result = different_books(nb_books)
+      expect(result).to eq value_expect
+    end
+
+    it "Three different books cost 24 euros" do
+      nb_books = 3
+      value_expect = 24
+      result = different_books(nb_books)
+      expect(result).to eq value_expect
+    end
+
+    it "Four different books cost 32 euros" do
+      nb_books = 4
+      value_expect = 32
+      result = different_books(nb_books)
+      expect(result).to eq value_expect
+    end
+
+    it "Five different books cost 40 euros" do
+      nb_books = 5
+      value_expect = 40
+      result = different_books(nb_books)
+      expect(result).to eq value_expect
+    end
   end
 
-  it "Two different books cost 16 euros" do
-    nb_books = 2
-    value_expect = 16
-    result = magic_calculator(nb_books)
-    expect(result).to eq value_expect
-  end
+
 end
